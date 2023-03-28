d1e25989283c9514bc2247d0bc83e53aeff703e7
mukesh <mukesh.choudhari@wisdmlabs.com>
2023-03-28 10:04:37 +0530

    segregated code in different files.

--- admin/partials/my-plugin-admin-display.php
+++ admin/partials/my-plugin-admin-display.php
@@ -14,3 +14,49 @@
 ?>
 
 <!-- This file should primarily consist of HTML with a little bit of PHP. -->
+<!--Add Input fields on Schedule Content Page-->
+<div class="wrap">
+		<h1>Schedule Content</h1>
+
+
+		<form class="myform" method="post">
+			<input type="hidden" name="action" value="cc_form">
+
+			<label for="date">Date:</label>
+			<input type="date" name="date" id="date" /><br />
+
+			<label for="occasion">Occasion:</label>
+			<input type="text" name="occasion" id="occasion" /><br />
+
+			<label for="post_title">Post Title:</label>
+			<input type="text" name="post_title" id="post_title" /><br />
+
+			<label for="author">Author:</label>
+			<select name="author" id="author" required>
+				<?php
+				$users = get_users(array(
+					'role' => 'author',
+					'fields' => array('ID', 'display_name')));
+				foreach ($users as $user) {
+					echo '<option value="' . $user->ID . '">' . $user->display_name . '</option>';
+				}
+				?>
+			</select><br>
+
+			<label for="reviewer">Reviewer:</label>
+			<select name="reviewer" id="reviewer" required>
+				<?php
+				$admins = get_users(array(
+					'role' => 'administrator',
+					'fields' => array('ID', 'display_name')
+				));
+				foreach ($admins as $admin) {
+					echo '<option value="' . $admin->ID . '">' . $admin->display_name . '</option>';
+				}
+				?>
+			</select><br>
+
+			<?php submit_button('Schedule Post'); ?>
+
+		</form>
+	</div>
--- includes/class-my-plugin.php
+++ includes/class-my-plugin.php
@@ -156,7 +156,8 @@ class My_Plugin {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
-
+		$this->loader->add_action('init',$plugin_admin, 'handle_my_form');
+		$this->loader->add_action('admin_menu',$plugin_admin, 'my_add_menu_pages');
 	}
 
 	/**
